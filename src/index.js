3d5b766b2746b8ebaecfe6b61e44606379f2cae8
davidnherran <davidnherran@gmail.com>
2020-06-19 23:33:27 -0500

    Configuración del servidor

---
+++ src/index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const engine = require("ejs-mate");
+const path = require("path");
+
+// Initializations
+const app = express();
+
+// Settings
+app.engine("ejs", engine);
+app.set("view engine", "ejs");
+app.set('views', path.join(__dirname, "views"))
+
+// Routes
+app.use(require('./routes/'))
+
+// Srarting the server
+app.listen(3000, () => {
+  console.log("Server on port 3000");
+});
